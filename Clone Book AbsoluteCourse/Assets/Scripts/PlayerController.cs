22d817e1a1b0fda297cf3c2f43b81851ba7b216b
YHKwon <ssugy2@gmail.com>
2022-07-18 01:56:24 +0900

    #1 이동시 애니매이션 추가
    전/후진 좌우이동 구현.
    지금 코드는 idle애니매이션이 중첩으로 계속 적용된다.
    이거 enum으로 하나 만들어서 상태 체크하면서 봐주는게 좋을 것 같다.

--- Clone Book AbsoluteCourse/Assets/Scripts/PlayerController.cs
+++ Clone Book AbsoluteCourse/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PlayerAnim
+{
+    public AnimationClip idle;
+    public AnimationClip runF;
+    public AnimationClip runB;
+    public AnimationClip runL;
+    public AnimationClip runR;
+
+}
+
+
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 10;
@@ -13,11 +25,18 @@ public class PlayerController : MonoBehaviour
 
     private Transform playerTransform;
     private Vector3 moveDirection;  // 벡터 노말라이즈 해야된다.(대각이동)
-    
+
+    public PlayerAnim playerAnim;   // 애니매이션 클립 모아놓은 것
+    public Animation anim;          // 플레이어 애니매이션 자체
 
     private void Start()
     {
         playerTransform = GetComponent<Transform>();
+
+        // 애니매이션 지정
+        anim = GetComponent<Animation>();
+        anim.clip = playerAnim.idle;
+        anim.Play();
     }
 
     private void Update()
@@ -29,5 +48,17 @@ public class PlayerController : MonoBehaviour
         moveDirection = Vector3.forward * v + Vector3.right * h;
         playerTransform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime, Space.Self);
         playerTransform.Rotate(Vector3.up * r * rotateSpeed * Time.deltaTime, Space.Self);
+
+        // 부드럽게 애니매이션 전환
+        if (v >= 0.1f)
+            anim.CrossFade(playerAnim.runF.name, 0.3f);
+        else if (v <= -0.1f)
+            anim.CrossFade(playerAnim.runB.name, 0.3f);
+        else if (h >= 0.1f)
+            anim.CrossFade(playerAnim.runR.name, 0.3f);
+        else if (h <= -0.1f)
+            anim.CrossFade(playerAnim.runL.name, 0.3f);
+        else
+            anim.CrossFade(playerAnim.idle.name, 0.3f); // 이렇게 제어하는 코드가 더 별로같은데. currentAnim상태 만들어서 제어요망
     }
 }
